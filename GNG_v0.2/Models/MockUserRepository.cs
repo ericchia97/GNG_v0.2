3b719481e9d7f3a1cfe8204774b11bfb156ea6d6
ericchia97 <ericchia508@gmail.com>
2020-09-22 18:19:58 +0800

    Add project files.

---
+++ GNG_v0.2/Models/MockUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GNG_v0._2.Models
+{
+    public class MockUserRepository : UserRepository
+    {
+        private List<User> UserList;
+
+        public MockUserRepository()
+        {
+
+        }
+        public User GetUser(int ID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GNG_v0.2/Models/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GNG_v0._2.Models
+{
+    public interface UserRepository
+    {
+        User GetUser(int ID);
+    }
+}
